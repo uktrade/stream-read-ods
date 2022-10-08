7dcc79f835c2cc85250ebe3aaf7705557ac14bd8
Michal Charemza <michal@charemza.name>
2022-10-08 10:46:23 +0100

    refactor: remove groupby pattern
    If going for the "manual iteration" pattern that allows delegating (which is
    particularly useful for table cells), we may as well do away with the
    sentinal/groupby pattern. This keeps the various numbers of patterns down.

--- stream_read_ods.py
+++ stream_read_ods.py
@@ -1,5 +1,4 @@
 from datetime import date, datetime
-from itertools import groupby
 
 from lxml import etree
 from stream_unzip import stream_unzip
@@ -45,15 +44,9 @@ def stream_read_ods(ods_chunks, chunk_size=65536):
             yield from chunks
 
     def get_sheets_and_rows(parsed_xml):
-        # It's slightly tricky to convert the event-based lxml parser to more useful
-        # "nested iterable" API. To do this, this generator yields tuples of
-        #
-        # (sheet, sheet_name, row)
-        #
-        # where sheet is a plain object() used as a sentinal value so a later call to
-        # groupby can then group rows into sheets
-        #
-        # The name of the sheet is not used since sheets can have identical names
+
+        ns_table = '{urn:oasis:names:tc:opendocument:xmlns:table:1.0}'
+        ns_office = '{urn:oasis:names:tc:opendocument:xmlns:office:1.0}'
 
         def value_error():
             raise ValueError()
@@ -76,14 +69,45 @@ def stream_read_ods(ods_chunks, chunk_size=65536):
             except ValueError:
                 return datetime.fromisoformat(value)
 
-        ns_table = '{urn:oasis:names:tc:opendocument:xmlns:table:1.0}'
-        ns_office = '{urn:oasis:names:tc:opendocument:xmlns:office:1.0}'
+        def clear_mem(event, element):
+            if event == 'end':
+                element.clear()
+                while element.getprevious() is not None:
+                    del element.getparent()[0]
 
-        # A sentinal per sheet, to be able to groupby
-        sheet = None
+        def table_rows(parsed_xml_it):
+            row = None
 
-        # The name of the sheet (exposed in the API)
-        sheet_name = None
+            while True:
+                try:
+                    event, element = next(parsed_xml_it)
+                except StopIteration:
+                    return
+
+                # Starting a row
+                if event == 'start' and f'{ns_table}table-row' == element.tag:
+                    row = []
+
+                # Ending a row
+                if event == 'end' and f'{ns_table}table-row' == element.tag:
+                    yield tuple(row)
+
+                # Starting a table cell
+                if event == 'start' and f'{ns_table}table-cell' == element.tag:
+                    value_type = element.attrib.get(f'{ns_office}value-type')
+                    row.append(
+                        None if value_type is None else \
+                        parse_boolean(element.attrib[f'{ns_office}boolean-value']) if value_type == 'boolean' else \
+                        parse_date(element.attrib[f'{ns_office}date-value']) if value_type == 'date' else \
+                        parse_float(element.attrib[f'{ns_office}value']) if value_type == 'float' else \
+                        '' if value_type == 'string' else \
+                        value_error()
+                    )
+
+                if event == 'end' and f'{ns_table}table' == element.tag:
+                    return
+
+                clear_mem(event, element)
 
         # We use manual iteration to be able to delegate iterating
         parsed_xml_it = iter(parsed_xml)
@@ -96,44 +120,13 @@ def stream_read_ods(ods_chunks, chunk_size=65536):
 
             # Starting a table
             if event == 'start' and f'{ns_table}table' == element.tag:
-                sheet = object()
                 sheet_name = element.attrib[f'{ns_table}name']
+                yield sheet_name, table_rows(parsed_xml_it)
 
-            # Starting a row
-            if event == 'start' and f'{ns_table}table-row' == element.tag:
-                row = []
-
-            # Ending a row
-            if event == 'end' and f'{ns_table}table-row' == element.tag:
-                yield sheet, sheet_name, tuple(row)
-
-            # Starting a table cell
-            if event == 'start' and f'{ns_table}table-cell' == element.tag:
-                value_type = element.attrib.get(f'{ns_office}value-type')
-                row.append(
-                    None if value_type is None else \
-                    parse_boolean(element.attrib[f'{ns_office}boolean-value']) if value_type == 'boolean' else \
-                    parse_date(element.attrib[f'{ns_office}date-value']) if value_type == 'date' else \
-                    parse_float(element.attrib[f'{ns_office}value']) if value_type == 'float' else \
-                    '' if value_type == 'string' else \
-                    value_error()
-                )
-
-            # Reduce memory usage
-            if event == 'end':
-                element.clear()
-                while element.getprevious() is not None:
-                    del element.getparent()[0]
-
-    def without_sheet_object(grouped_sheets_and_rows):
-        for (_, sheet_name), rows in grouped_sheets_and_rows:
-            yield sheet_name, (row for (_, _, row) in rows)
+            clear_mem(event, element)
 
     unzipped_member_files = stream_unzip(ods_chunks, chunk_size=chunk_size)
     content_xml_chunks = get_member_file(b'content.xml', unzipped_member_files)
     content_xml_file_like_obj = to_file_like_obj(content_xml_chunks)
     content_xml_parsed = etree.iterparse(content_xml_file_like_obj, events=('start', 'end'))
-    sheets_and_rows = get_sheets_and_rows(content_xml_parsed)
-    grouped_sheets_and_rows = groupby(sheets_and_rows, lambda row: (row[0], row[1]))
-
-    yield from without_sheet_object(grouped_sheets_and_rows)
+    yield from get_sheets_and_rows(content_xml_parsed)
